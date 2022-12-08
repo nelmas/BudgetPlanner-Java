0e6faa94f9ef1766a9061a600178ce4df4e922ab
nelmas <an3744ma-s@student.lu.se>
2022-12-08 13:28:40 +0100

    Created enums for teacher titles (vet ej om det behövs egentligen?)

--- src/Teacher.java
+++ src/Teacher.java
@@ -23,8 +23,16 @@ public class Teacher {
 		this.taught = new ArrayList<Course>();
 		
 	}
-	//Instances
+	//Titles
+	enum Titles {
+		LECTURER,
+		ASSISTANTPROFESSOR,
+		ASSOCIATEPROFESSOR,
+		PROFESSOR,
 	
+	//Titles title = Titles.ASSISTANTPROFESSOR;
+		//System.out.println(title);
+	}
 	
 	//Getters & Setters
 	public ArrayList<Course> getTaught() {
