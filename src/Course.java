ba575728710661b90eb8b3b60a55876cef4d4ce2
astridandersson <117929376+astridandersson@users.noreply.github.com>
2022-12-08 12:40:54 +0100

    Updates on Course
    added some methods

--- src/Course.java
+++ src/Course.java
@@ -5,24 +5,33 @@ private String name;
 private String courseCode;
 private int credits;
 private String cycle;
+private Teacher responsible;
 private ArrayList <Teacher> teaches;
 
 
+
+//constructors
 public Course() {
-	this.teaches = new ArrayList<Teacher>(); 
+	this.setTeaches(new ArrayList<Teacher>()); 
 
 }
 
-public Course(String name, String courseCode, int credits, String cycle) {
+public Course(String name, String courseCode, int credits, String cycle, Teacher responsible) {
  this.name = name;
  this.courseCode = courseCode;
  this.credits = credits;
  this.cycle = cycle;
- this.teaches = new ArrayList<Teacher>(); 
-
+ this.responsible = responsible;
+ this.setTeaches(new ArrayList<Teacher>()); 
 
 }
 
+
+//instansiera andra klasser
+Teacher teacher = new Teacher();
+
+
+//Getters & Setters
 public String getName() {
 	return name;
 }
@@ -54,10 +63,61 @@ public String getCycle() {
 public void setCycle(String cycle) {
 	this.cycle = cycle;
 }
+
+public Teacher getResponsible() {
+	return responsible;
+}
+
+public void setResponsible(Teacher responisble) {
+	this.responsible = responisble;
+}
+
+public ArrayList <Teacher> getTeaches() {
+	return teaches;
+}
+
+public void setTeaches(ArrayList <Teacher> teaches) {
+	this.teaches = teaches;
+}
+
+//Methods
+
+public void createCorse(){
+		
+}
+
+public void readCorse(){
 	
+	
+}
+public void deleteCorse(){
+		
+}
+public void displayAllTeachers() {
+for (int i = 0; i>teaches.size(); i++ ) {
+	teacher.getName();
+	teacher.getEmployeeId();
+}
+
+}
+public void displayResponsible() {
+	
+}
+
+public void addTeacher(Teacher teacher) {
+	this.teaches.add(teacher);
+}
+public void removeTeacher(Teacher teacher) {
+	this.teaches.remove(teacher);
+	
+}
+public void addResponsible() {
+	
+}
+public void removeResponsible() {
+}
 
 
-
-	}
+}
 
 
