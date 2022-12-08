6f1c05d1bdba5941df269f3cb927b52dc51c4c73
nelmas <an3744ma-s@student.lu.se>
2022-12-08 11:34:29 +0100

    created Teacher class with setters and getters

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,9 +1,50 @@
 
 public class Teacher {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String employeeId;
+	private String name;
+	private String title;
+	private String address;
+	private int hourlySalary;
 
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getHourlySalary() {
+		return hourlySalary;
+	}
+
+	public void setHourlySalary(int hourlySalary) {
+		this.hourlySalary = hourlySalary;
 	}
 
 }
