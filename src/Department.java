248b766c26b2332a6fcdc7050493b5e7e2e69394
nelmas <an3744ma-s@student.lu.se>
2022-12-12 14:21:44 +0100

    removed import assign4
    how did they even get there!?!?! :-1: :angry:

--- src/Department.java
+++ src/Department.java
@@ -1,14 +1,9 @@
 import java.util.ArrayList;
 
-import assignment4.New.BankAccount;
-import assignment4.New.Person;
-
-
-
 public class Department {
-	
-   //Declare attributes
-	
+
+	// Declare attributes
+
 	private String name;
 	private Teacher works;
 	private String address;
@@ -16,11 +11,11 @@ public class Department {
 	private ArrayList<Teacher> teacher;
 	private ArrayList<Department> department;
 
-    //constructors
-	
-	public Department () {
-		}
-	
+	// constructors
+
+	public Department() {
+	}
+
 	public Department(String name, String address, int budget) {
 		this.address = address;
 		this.budget = budget;
@@ -28,30 +23,30 @@ public class Department {
 		this.teacher = new ArrayList<Teacher>();
 		this.department = new ArrayList<Department>();
 	}
-	
-	//getters and setters
-	
+
+	// getters and setters
+
 	public Teacher getWorks() {
 		return works;
 	}
 
-
-
 	public void setWorks(Teacher works) {
 		this.works = works;
 	}
+
 	public ArrayList<Teacher> getTeacher() {
 		return teacher;
 	}
+
 	public void setTeacher(ArrayList<Teacher> teacher) {
 		this.teacher = teacher;
-		
+
 	}
+
 	public ArrayList<Department> getDepartment() {
 		return department;
 	}
-	
-	
+
 	public Department findDepartment(String name) {
 		for (Department department : department) {
 			if (department.findDepartment(name) != null) {
@@ -61,49 +56,52 @@ public class Department {
 		}
 		return null;
 	}
-	
-	
 
 	public void setDepartment(ArrayList<Department> department) {
 		this.department = department;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public int getBudget() {
 		return budget;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public void setBudget(int budget) {
 		this.budget = budget;
 	}
-	
-	
-	//add teacher method
+
+	// add teacher method
 	public void addTeacher(Teacher teacher) {
 		this.teacher.add(teacher);
 	}
-	
-	//remove teacher method
+
+	// remove teacher method
 	public void removeTeacher(Teacher teacher) {
 		this.teacher.remove(teacher);
-		
+
 	}
-	
-	//add department
+
+	// add department
 	public void addDepartment(Department department) {
 		this.department.add(department);
 	}
-	//remove department
+
+	// remove department
 	public void removeDepartment(Department department) {
 		this.department.remove(department);
 	}
