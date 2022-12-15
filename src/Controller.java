7e7d40f60ffc5917b8dc22ff748b2d6198ef9399
astridandersson <117929376+astridandersson@users.noreply.github.com>
2022-12-15 14:00:09 +0100

    update

--- src/Controller.java
+++ src/Controller.java
@@ -163,7 +163,7 @@ public class Controller implements ActionListener {
 				
 				}
 		);
-		
+		 
 	
 
 				
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
 
-import assignment4.New.BankAccount;
-import assignment4.New.Person;
-
 
 public class Teacher {
 
